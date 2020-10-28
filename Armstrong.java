a23b433966e90c778ba02d1470ad2cf574a7ac1d
Goutham-Krishnan <72788005+Goutham-Krishnan@users.noreply.github.com>
2020-10-28 10:38:48 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Armstrong {
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=scan.nextInt();
+		Armstrong obj=new Armstrong();
+		if(n==obj.check(n)){
+			System.out.println(n+" is armstrong");
+		}
+		else{
+			System.out.println(n+" is not armstrong");
+		}
+		scan.close();
+	}
+	
+	int check(int n){
+		int sum=0;
+		while(n>0){
+			sum+=java.lang.Math.pow((n%10),3);
+			n=n/10;
+		}
+		return sum;
+	}
+}
