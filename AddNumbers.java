7b452b995e48e2d15faf00f71d9e15ddaa311f3b
Goutham-Krishnan <72788005+Goutham-Krishnan@users.noreply.github.com>
2020-10-28 10:10:14 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class AddNumbers {
+	public static void main(String args[]){
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the numbers");
+		int n1=scan.nextInt();
+		int n2=scan.nextInt();
+		System.out.println(n1+n2);
+		scan.close();
+	}
+
+}
